cdb6cf4152a35a12bf98e164b2732612ce1bb314
JPromi <jonasmpr05@gmail.com>
2024-03-13 11:19:32 +0100

    remove server url

--- dependency.py
+++ dependency.py
@@ -6,7 +6,7 @@ import os
 
 def is_local_ip(request: Request):
     # return request.client.host == socket.gethostbyname(socket.gethostname())
-    return request.client.host in ["192.168.178.38", socket.gethostbyname(socket.gethostname())]
+    return request.client.host in [socket.gethostbyname(socket.gethostname())]
 
 def get_os():
     return os.name
