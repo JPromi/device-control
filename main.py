62481d75cf05b09a10a86fccf0783940fb362db7
JPromi <jonasmpr05@gmail.com>
2024-04-11 14:31:53 +0200

    fix cors

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI, HTTPException
 from fastapi.responses import Response
 from starlette.requests import Request
 from controller import ping_router, power_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(ping_router, prefix="/ping")
 app.include_router(power_router, prefix="/system/power")
